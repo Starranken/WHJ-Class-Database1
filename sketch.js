3ce0da8c4494cce750c0ca06959d053af3438b30
Starranken <72114247+Starranken@users.noreply.github.com>
2020-12-08 17:59:26 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var ball;
+var database, position;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value", readPosition);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        x: position.x + x,
+        y: position.y + y
+    })
+}
